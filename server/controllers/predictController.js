f29cd7ec1f589c4a2efbd0721be58f265c668257
meanish <anishgshrestha@gmail.com>
2024-12-21 23:29:32 +0545

    small changes

--- server/controllers/predictController.js
+++ server/controllers/predictController.js
@@ -3,16 +3,17 @@ const Predict = require("../services/predictService");
 
 
 const findprice = async (req, res) => {
+    res.status(200).json({ data: "okay", success: true });
 
-    try {
+    // try {
 
-        const predictStatus = await Predict.find(req,res)
-        console.log("OPredict", predictStatus)
-        if (predictStatus?.success)
-            res.status(200).json({ data: "okay", success: true });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-    }
+    //     const predictStatus = await Predict.find(req,res)
+    //     console.log("OPredict", predictStatus)
+    //     if (predictStatus?.success)
+    //         res.status(200).json({ data: "okay", success: true });
+    // } catch (error) {
+    //     res.status(500).json({ success: false, error: error.message });
+    // }
 }
 
 module.exports = {
