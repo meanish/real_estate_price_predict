34f661219d159e3da3910bcc91e33e73d18ba3f1
Anish Gho Shrestha <95839154+meanish@users.noreply.github.com>
2025-01-16 10:02:21 +0545

    Update predictService.js

--- server/services/predictService.js
+++ server/services/predictService.js
@@ -2,14 +2,14 @@ const { spawn } = require('child_process');
 const path = require('path');
 
 const find = (req, res) => {
-    const { location, sqft, bath, bhk } = req.body;  // Get data from request body
+    const { location, sqft, bath, bhk } = req.body; 
     console.log(location);
     console.log('Current directory:', __dirname);
     const pythonFilePath = path.join(__dirname, '../../model/predict.py');
-    console.log('Python script path:', pythonFilePath); // Log the full path to the Python script
+    console.log('Python script path:', pythonFilePath); 
 
     // Spawn the Python process
-    const pythonProcess = spawn('python', [pythonFilePath, location, sqft, bath, bhk]); // Pass arguments to Python script
+    const pythonProcess = spawn('python', [pythonFilePath, location, sqft, bath, bhk]); 
 
     // Handle Python script output
     pythonProcess.stdout.on('data', (data) => {
@@ -18,20 +18,20 @@ const find = (req, res) => {
             // Parse the JSON data returned by Python
             const response = JSON.parse(data.toString());
             console.log("Response:", response);
-            res.json({ success: true, data: response });  // Send response back to Postman
+            res.json({ success: true, data: response });  
         } catch (error) {
             console.error('Error parsing Python response:', error);
             res.status(500).json({ success: false, message: 'Error parsing Python response' });
         }
     });
     
-    // Handle Python script error
+   
     pythonProcess.stderr.on('data', (data) => {
         console.error('Python script error:', data.toString());
         res.status(500).json({ success: false, message: `Python error: ${data.toString()}` });
     });
 
-    // Handle Python process close (exit)
+ 
     pythonProcess.on('close', (code) => {
         console.log(`Python process exited with code ${code}`);
         if (code !== 0) {
